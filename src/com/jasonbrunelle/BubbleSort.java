4d1b03f6aadeca1d3ade0618e73627618289efe1
Jason Brunelle <jason@jasonbrunelle.com>
2019-10-21 16:07:13 -0500

    Redoing last commit to add more files that weren't added before.

--- src/com/jasonbrunelle/BubbleSort.java
+++ src/com/jasonbrunelle/BubbleSort.java
@@ -1,19 +1,5 @@
 package com.jasonbrunelle;
 
 public class BubbleSort {
-    public static int[] bubbleSort(int[] arr) {
-        boolean swapped = true;
-        while (swapped) {
-            swapped = false;
-            for (int i = 0; i < arr.length - 1; i++) {
-                if (arr[i] > arr[i+1]) {
-                    int temp = arr[i];
-                    arr[i] = arr[i+1];
-                    arr[i+1] = temp;
-                    swapped = true;
-                }
-            }
-        }
-        return arr;
-    }
+
 }
--- src/com/jasonbrunelle/QuickSort.java
+++ src/com/jasonbrunelle/QuickSort.java
@@ -1,43 +1,5 @@
 package com.jasonbrunelle;
 
 public class QuickSort {
-    public static int[] quickSort(int[] arr) {
-        // First of recursive call
-        return quickSort(arr, 0, arr.length-1);
-    }
 
-    private static int[] quickSort(int[] arr, int left, int right) {
-        int index = partition(arr, left, right);
-        if (left < index - 1) { // Sort left half
-            quickSort (arr, left, index - 1);
-        }
-        if (index < right) { // Sort right half
-            quickSort(arr, index, right);
-        }
-        return arr;
-    }
-
-    private static int partition(int[] arr, int left, int right) {
-        int pivot = arr[(left + right) / 2]; // Pick a pivot point
-        while (left <= right) {
-            // Find element on the lft that should be on the right
-            while (arr[left] < pivot) left++;
-
-            //Find element on the right that should be on the left
-            while (arr[right] > pivot) right--;
-
-            // Swap elements, and move left and right indices
-            if (left <= right) {
-                // Swap elements
-                int temp = arr[left];
-                arr[left] = arr[right];
-                arr[right] = temp;
-
-                // Move indices
-                left++;
-                right--;
-            }
-        }
-        return left;
-    }
 }
